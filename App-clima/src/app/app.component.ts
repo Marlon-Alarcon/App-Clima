6c8ded5a8616dd727c4e3e301e6cafc238f98132
Marlon-Alarcon <89886882+Marlon-Alarcon@users.noreply.github.com>
2023-11-16 19:55:56 -0500

    json estatico

--- App-clima/src/app/app.component.ts
+++ App-clima/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'App-clima';
+export class AppComponent implements OnInit{
+
+  jsonData: any = [];
+  selectedCountry: string = "";
+
+  constructor(
+    private weatherservice: WeatherService,
+    private pais: WeatherService
+  ){}
+
+
+  ngOnInit(){
+    this.weatherservice.getweather('riohacha','co').subscribe((data)=>{
+      console.log(data,"Datos")
+    },error => console.error(error))
+
+
+    this.pais.getpaises().subscribe((data)=>{
+      this.jsonData = data
+
+      console.log(this.jsonData,"Toda la data")
+    },error => console.error(error))
+  }
+
+  sendData(city:any,country:any){
+    console.log(city.value)
+    console.log(country.value)
+    return false
+  }
 }
--- App-clima/src/app/services/weather.service.ts
+++ App-clima/src/app/services/weather.service.ts
@@ -11,13 +11,17 @@ export class WeatherService {
   key = '0c4979dd6605b4e2c2fbddedb517ddc5'
 
 constructor(private httpclient : HttpClient) { 
-  this.URL = `https://api.openweathermap.org/data/2.5/weather?appid=`
+  this.URL = `https://api.openweathermap.org/data/2.5/weather?appid=${this.key}`
 }
 
 getweather(ciudad:string, pais:string){
   
   return this.httpclient.get(`${this.URL}&q=${ciudad},${pais}`)
 
+}
+
+getpaises(){
+  return this.httpclient.get<any[]>('assets/json/JsonPaises.json');
 }
 
 }
